81609dead9e7e2c8a3ae97276849f4d4002792a3
leokun <131544788+leookun@users.noreply.github.com>
2025-07-11 22:32:10 +0800

    Update driver_sign_radius.js

--- driver_sign_radius.js
+++ driver_sign_radius.js
@@ -8,7 +8,7 @@ let body = $response.body;
 
 // 使用正则表达式替换所有的 "radius": 数值, 为 "radius": 1000000,
 // 匹配 "radius": 后面的数值直到逗号
-body = body.replace(/"radius":\s*(.+),/g, '"radius": 1000000,');
+body = body.replace(/"radius":\s+(.+),/g, '"radius": 1000000,');
 
 // 输出修改后的响应体
 $done({body}); 
