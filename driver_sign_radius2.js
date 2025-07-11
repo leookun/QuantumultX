71253b6afbf7f2587eabdc3d05d01e768b90d39b
leokun <131544788+leookun@users.noreply.github.com>
2025-07-11 22:44:12 +0800

    Update driver_sign_radius3.js

--- driver_sign_radius2.js
+++ driver_sign_radius2.js
@@ -1,5 +1,5 @@
 let body = $response.body;
-body = body.replace(/"radius":\s+(.+),/g, '"radius": 1000000,');
+body = body.replace(/"radius":\s*[\d.]+/g, '"radius": 100000000');
 console.log(body);
 // 输出修改后的响应体
 $done({body}); 
